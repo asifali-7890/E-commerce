b864f71093ec7ae2d6051b23533854140d611916
asifali-7890 <78083087+asifali-7890@users.noreply.github.com>
2025-03-12 23:02:48 +0530

    last commit

--- server/index.js
+++ server/index.js
@@ -6,7 +6,29 @@ import connectDB from './config/DB.js';
 
 dotenv.config();
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 const app = express();
+
+// Get the directory name for ES6 modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// console.log('JWT_SECRET:', process.env.JWT_SECRET); // Log the JWT_SECRET to check if it's loaded
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+// Catch-all route to serve the React app's index.html for any other route
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+});
+
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
